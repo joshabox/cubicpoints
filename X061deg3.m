f2fa29e31c9b1ad5fac08b9eeddbc439b2d27a7e
PipGoodman <62069879+PipGoodman@users.noreply.github.com>
2020-12-25 18:37:52 +0000

    Update X061deg3.m

--- X061deg3.m
+++ X061deg3.m
@@ -67,13 +67,12 @@ tf, phi := IsIsomorphic(F,K);
 assert tf;
 X(K)![phi(coef): coef in Eltseq(Pt)];
 phi(j(Pt));
-EPt := EllipticCurveFromjInvariant(j(Pt));
-assert not HasComplexMultiplication(EPt);
-L := NormalClosure(F);
-GaloisConjugates :=[X(L)![sigma(coef): coef in Eltseq(Pt)]: sigma in Automorphisms(L)];
-CondEPt := Conductor(EllipticCurveFromjInvariant(j(GaloisConjugates[1])));
-assert &and[Parent(CondEPt) eq Parent(Conductor(EllipticCurveFromjInvariant(j(sigmaPt)))): sigmaPt in GaloisConjugates]; // sanity check
-assert not &and[CondEPt eq Conductor(EllipticCurveFromjInvariant(j(sigmaPt))): sigmaPt in GaloisConjugates];
+tf, D := CMorQcurve(j(Pt));
+if tf then
+"Has CM by", D;
+else assert not tf;
+"Is not a Q-curve";
+end if;
 end for;
 
 
